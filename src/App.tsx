b8b9f5f40027dc4349c8948c021f4802fcc0a922
Rageed173 <s12323203@gmail.com>
2026-06-22 10:59:17 +0300

    creating form for classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
+import {  Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -13,10 +13,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
 import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+import ClassesList from "./pages/classes/list";
+import ClassesCreate from "./pages/classes/create";
 function App() {
   return (
     <BrowserRouter>
@@ -43,7 +45,13 @@ function App() {
                       list: '/subjects',
                       create: '/subjects/create',
                       meta: { lable: 'Subjects', icon: <BookOpen/> },
-                  }
+                  },
+                  {
+                    name: 'classes',
+                    list: '/classes',
+                    create: '/classes/create',
+                    meta: { lable: 'Classes', icon: <GraduationCap/> },
+                }
               ]
               }
             >
@@ -58,7 +66,10 @@ function App() {
                           <Route index element={<SubjectsList/>} />
                           <Route path="create" element={<SubjectsCreate/>} />
                       </Route>
-
+                      <Route path="classes">
+                          <Route index element={<ClassesList/>} />
+                          <Route path="create" element={<ClassesCreate/>} />
+                      </Route>
                   </Route>
               </Routes>
               <Toaster />
